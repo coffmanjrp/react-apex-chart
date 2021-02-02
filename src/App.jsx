c74d1d3cf0df0029a1959f6d7b468041e127903f
coffmanjrp <coffmanjrp@gmail.com>
2021-02-02 15:53:27 -0800

    create popchart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hello</h1>
     </div>
   );
---
+++ src/components/PopChart.jsx
@@ -0,0 +1,5 @@
+const PopChart = () => {
+  return <div></div>;
+};
+
+export default PopChart;
